a4774c153614516b568bf9b9fed08020eb613abb
kaioufcg <kaio.ufcg@gmail.com>
2012-04-03 11:27:36 -0300

    commit numero 2
    Neste ja começo a implementação em relação aos teste de aceitação do
    easy accept!

--- ProjetoSI1/src/projeto/Projeto.java
+++ ProjetoSI1/src/projeto/Projeto.java
@@ -1,5 +1,105 @@
 package projeto;
 
 public class Projeto {
+	private String login, senha, nome, endereco, email;
+
+	public Projeto() {
+
+	}
+
+	/*public void criarUsuario(String login, String senha, String nome,
+			String endereco, String email) {
+		this.login = login;
+		this.senha = senha;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.email = email;
+
+	}
+	*/
+
+	public void zerarSistema() {
+		// TODO Auto-generated method stub
+	}
+
+	public void getAtributoUsuario(String login, String atributo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @return the senha
+	 */
+	public String getSenha() {
+		return senha;
+	}
+
+	/**
+	 * @param senha
+	 *            the senha to set
+	 */
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	/**
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/**
+	 * @param login
+	 *            the login to set
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome
+	 *            the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the endereco
+	 */
+	public String getEndereco() {
+		return endereco;
+	}
+
+	/**
+	 * @param endereco
+	 *            the endereco to set
+	 */
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
---
+++ ProjetoSI1/src/testesProject/Teste.java
@@ -0,0 +1,51 @@
+package testesProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import projeto.Projeto;
+import easyaccept.EasyAcceptFacade;
+
+/**
+ * UFCG - CEEI - DSC Disciplina: Sistema de Informacao I. Professor: Nazareno.
+ * 
+ * Projeto SI1 2012.1.
+ * 
+ * Pacote testesProject Classe Main
+ * 
+ * Classe para execucao dos testes do EasyAcceppt.
+ * 
+ * @author Grupo 6.
+ * @version 1.0.0
+ * 
+ */
+
+public class Teste {
+
+	public static void main(String[] args) throws Exception {
+
+		List<String> files = new ArrayList<String>();
+
+		files.add("testesProjectEasyAcceptFacade/US01.txt");
+		// files.add("testesProjectEasyAcceptFacade/US02.txt");
+		// files.add("testesProjectEasyAcceptFacade/US03.txt");
+		// files.add("testesProjectEasyAcceptFacade/US04.txt");
+		// files.add("testesProjectEasyAcceptFacade/US05.txt");
+
+		Projeto projeto = new Projeto();
+
+		// Instantiate EasyAccept façade
+
+		EasyAcceptFacade eaFacade = new EasyAcceptFacade(projeto, files);
+
+		// Execute the tests
+
+		eaFacade.executeTests();
+
+		// Print the tests execution results
+
+		System.out.println(eaFacade.getCompleteResults());
+
+	}
+
+}
